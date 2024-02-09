133620560d1d7678d74718e0a85262b7e25747c8
Dev-Farhana <code.nana22@gmail.com>
2024-02-09 21:17:31 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+gsap.from("#tooth-icon", { y: -45, duration: 3 });
+
+gsap.from(".h1", { y: -45, duration: 3 });
+
+// gsap.fromTo(".h1", {y: -45},{ x: 10 , duration: 3});
+
+// document.addEventListener("mousemove", function(event) {
+//     console.log("Mouse X: " + event.clientX); 
+//     console.log("Mouse Y: " + event.clientY);
+//   });
+  
+//   y :1-45;
+// x: 430;
